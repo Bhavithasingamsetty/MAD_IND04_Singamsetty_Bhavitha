50ecc5bd870923980a908bd480ba96eb5917959b
BhavithaSingamsetty <singamsettybhavitha@gmail.com>
2024-04-20 15:07:15 -0500

    3:07pm

---
+++ MAD_Ind04_Singamsetty_Bhavitha/MAD_Ind04_Singamsetty_Bhavitha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAD_Ind04_Singamsetty_Bhavitha
+//
+//  Created by Singamsetty, Bhavitha on 4/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("HI")
+    }
+
+
+}
+
---
+++ MAD_Ind04_Singamsetty_Bhavitha/MAD_Ind04_Singamsetty_BhavithaUITests/MAD_Ind04_Singamsetty_BhavithaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MAD_Ind04_Singamsetty_BhavithaUITestsLaunchTests.swift
+//  MAD_Ind04_Singamsetty_BhavithaUITests
+//
+//  Created by Singamsetty, Bhavitha on 4/20/24.
+//
+
+import XCTest
+
+final class MAD_Ind04_Singamsetty_BhavithaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
