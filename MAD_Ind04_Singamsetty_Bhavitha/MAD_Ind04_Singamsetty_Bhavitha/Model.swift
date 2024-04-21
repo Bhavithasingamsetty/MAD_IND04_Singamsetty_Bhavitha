42dd623943f1934b801f44698791ce0004a9b342
BhavithaSingamsetty <singamsettybhavitha@gmail.com>
2024-04-21 18:40:12 -0500

    6:40pm 21april

---
+++ MAD_Ind04_Singamsetty_Bhavitha/MAD_Ind04_Singamsetty_Bhavitha/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  MAD_Ind04_Singamsetty_Bhavitha
+//
+//  Created by Singamsetty, Bhavitha on 4/21/24.
+//
+
+import Foundation
---
+++ MAD_Ind04_Singamsetty_Bhavitha/MAD_Ind04_Singamsetty_Bhavitha/View.swift
@@ -0,0 +1,8 @@
+//
+//  View.swift
+//  MAD_Ind04_Singamsetty_Bhavitha
+//
+//  Created by Singamsetty, Bhavitha on 4/21/24.
+//
+
+import Foundation
